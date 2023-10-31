c0f2bcb7324452ae42fbce35dcbf2fa171a80199
JimmyVin <jimmy-dragon1@hotmail.com>
2023-10-31 18:24:28 -0500

    'inicio'

---
+++ src/main/java/com/inicio/jimmy/JimmyApplication.java
@@ -0,0 +1,13 @@
+package com.inicio.jimmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inicio/jimmy/JimmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inicio.jimmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
