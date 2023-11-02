3a50f612ffb4c3e0dd58acbc03ef18620973e0e7
JimmyVin <jimmy-dragon1@hotmail.com>
2023-11-01 20:26:53 -0500

    'aplication'

--- src/main/java/com/inicio/jimmy/JimmyApplication.java
+++ src/main/java/com/inicio/jimmy/JimmyApplication.java
@@ -2,9 +2,25 @@ package com.inicio.jimmy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
+@Controller
 public class JimmyApplication {
+
+	@GetMapping("/users")
+	public String listUsers() {
+		// Código para mostrar la lista de usuarios
+		return "users";
+	}
+
+	@PostMapping("/users")
+	public String createUser() {
+		// Código para crear un usuario
+		return "redirect:/users";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JimmyApplication.class, args);
